ab51e5f4c1a3d99ed63763506335464bc4e515da
boggard <varyanik2@yandex.ru>
2019-02-12 22:07:25 +0300

    feat: spring web app structure

---
+++ src/main/java/com/github/cydb/CydbApplication.java
@@ -0,0 +1,14 @@
+package com.github.cydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CydbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CydbApplication.class, args);
+    }
+
+}
+
