da6cc880261109346929b48616291aaa308e50c4
boggard <varyanik2@yandex.ru>
2019-03-19 15:30:43 +0300

    feat: implement sql parser

---
+++ src/main/java/com/github/boggard/cydb/parser/SQLParser.java
@@ -0,0 +1,33 @@
+package com.github.boggard.cydb.parser;
+
+import com.github.boggard.cydb.antlr.SqlLexer;
+import com.github.boggard.cydb.antlr.SqlListener;
+import com.github.boggard.cydb.antlr.SqlParser;
+import lombok.experimental.UtilityClass;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.springframework.core.io.InputStreamSource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@UtilityClass
+public class SQLParser {
+
+    public void parseSql(InputStreamSource inputStreamSource, SqlListener sqlListener) throws IOException {
+        try (InputStream inputStream = inputStreamSource.getInputStream()) {
+            ANTLRInputStream antlrInputStream = new ANTLRInputStream(inputStream);
+
+            SqlLexer sqlLexer = new SqlLexer(antlrInputStream);
+            CommonTokenStream tokenStream = new CommonTokenStream(sqlLexer);
+            SqlParser sqlParser = new SqlParser(tokenStream);
+
+            ParseTree parseTree = sqlParser.parse();
+
+            ParseTreeWalker walker = new ParseTreeWalker();
+            walker.walk(sqlListener, parseTree);
+        }
+    }
+}
