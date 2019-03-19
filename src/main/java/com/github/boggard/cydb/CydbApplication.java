af2f2f46aff71643ec24344c60ccb86d7ad81a8d
boggard <varyanik2@yandex.ru>
2019-03-19 12:00:07 +0300

    refactor: package name changed

--- src/main/java/com/github/boggard/cydb/CydbApplication.java
+++ src/main/java/com/github/boggard/cydb/CydbApplication.java
@@ -1,4 +1,4 @@
-package com.github.cydb;
+package com.github.boggard.cydb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
