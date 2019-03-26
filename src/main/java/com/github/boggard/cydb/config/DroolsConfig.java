7274fa35e24f1722e5d00ae2480ffeb32d89c654
boggard <varyanik2@yandex.ru>
2019-03-26 12:31:25 +0300

    feat: configure drools

---
+++ src/main/java/com/github/boggard/cydb/config/DroolsConfig.java
@@ -0,0 +1,55 @@
+package com.github.boggard.cydb.config;
+
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.KieRepository;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.io.ResourceFactory;
+import org.kie.spring.KModuleBeanFactoryPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DroolsConfig {
+
+    @Bean
+    public KieFileSystem kieFileSystem() {
+        KieFileSystem kieFileSystem = getKieServices().newKieFileSystem();
+        kieFileSystem.write(ResourceFactory.newClassPathResource("rules.drl", "UTF-8"));
+        return kieFileSystem;
+    }
+
+    @Bean
+    public KieContainer kieContainer() {
+        final KieRepository kieRepository = getKieServices().getRepository();
+
+        kieRepository.addKieModule(kieRepository::getDefaultReleaseId);
+
+        KieBuilder kieBuilder = getKieServices().newKieBuilder(kieFileSystem());
+        kieBuilder.buildAll();
+
+        return getKieServices().newKieContainer(kieRepository.getDefaultReleaseId());
+    }
+
+    private KieServices getKieServices() {
+        return KieServices.Factory.get();
+    }
+
+    @Bean
+    public KieBase kieBase() {
+        return kieContainer().getKieBase();
+    }
+
+    @Bean
+    public KieSession kieSession() {
+        return kieContainer().newKieSession();
+    }
+
+    @Bean
+    public KModuleBeanFactoryPostProcessor kiePostProcessor() {
+        return new KModuleBeanFactoryPostProcessor();
+    }
+}
